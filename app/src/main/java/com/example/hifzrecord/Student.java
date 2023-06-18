e90bb4450c997673bb15e0e7a6db257faacf6c6c
ali <ahmadmubashar373@gmail.com>
2023-06-18 13:22:34 +0500

    Student class added

---
+++ app/src/main/java/com/example/hifzrecord/Student.java
@@ -0,0 +1,50 @@
+package com.example.hifzrecord;
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String Class;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, int age, String Class){
+        this.id = id;
+        this.name=name;
+        this.age=age;
+        this.Class = Class;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getClasss(){
+        return Class;
+    }
+
+    public void setClass(String Class) {
+        this.Class = Class;
+    }
+}
